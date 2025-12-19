f3c7b025eaf6aee14312a88f847de80d3e6b8d5e
Sayed Sifat Zoha <syedsifat.dgr@gmail.com>
2025-12-20 00:19:18 +0530

    Add main application structure and sections components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import ParticlesBackground from "./components/ParticlesBackground";
+import Navbar from "./components/Navbar";
+import About from "./sections/About";
+import Experiance from "./sections/Experiance";
+import Home from "./sections/Home";
+import Project from "./sections/Project";
+import Skills from "./sections/Skills";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 
-function App() {
- 
+
+
+export default function App() {
   return (
-    <>
-      
-    </>
+    <div className = "relative gradient text-white">
+    <ParticlesBackground/>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Skills/>
+    <Project/>
+    <Experiance/>
+    <Testimonials/>
+    <Contact/>
+    <Footer/>  
+
+    </div>
   )
 }
-
-export default App
---
+++ src/sections/Experiance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Experiance = () => {
+  return (
+    <div className='w-full h-screen'>
+      Experiance
+    </div>
+  )
+}
+
+export default Experiance
